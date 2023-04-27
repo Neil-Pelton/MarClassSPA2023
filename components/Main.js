40f7d127ae0909c20c1eb8a3c1548d8a1da9cc52
Neil-Pelton <neilpel22@gmail.com>
2023-04-26 21:22:52 -0500

    Updated SPA week6

--- components/Main.js
+++ components/Main.js
@@ -2,9 +2,4 @@
 import html from "html-literal";
 import * as views from "./views"; //creating object called views, each item in views are features we're importing
 
-export default () => html`
-  ${views["Home"]()},
-  ${views["Bio"]()},
-  ${views["Pizza"]()},
-  ${views["Order"]()}
-`;
+export default (state) => html` ${views[state.view](state)} `;
--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 import { Header, Nav, Main, Footer } from "./components";
+import * as store from "./store";
+import Navigo from "navigo";
+import { after, capitalize } from "lodash";
 
-function render() {
-  document.querySelector(`#root`).innerHTML = `
-  ${Header()}
-  ${Nav()}
-  ${Main()}
-  ${Footer()}
+const router = new Navigo("/");
+
+function render(state = store.Home) {
+  document.querySelector("#root").innerHTML = `
+    ${Header(state)}
+    ${Nav(store.Links)}
+    ${Main(state)}
+    ${Footer()}
   `;
+
+  afterRedner(state);
+
+  router.updatePageLinks();
 }
 
-render();
+function afterRedner(state) {
+  //add menu toggle to bars icon in nav bar
+  document.querySelector(".fa-bars").addEventListener("click", () => {
+    document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+  });
+}
 
-
-// add menu toggle to bars icon in nav bar
-document.querySelector(".fa-bars").addEventListener("click", () => {
-  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-});
+router
+  .on({
+    "/": () => render(),
+    ":view": params => {
+      let view = capitalize(params.data.view);
+      if (store.hasOwnProperty(view)) {
+        render(store[view]);
+      } else {
+        console.log(`View ${view} not defined`);
+      }
+    }
+  })
+  .resolve();
