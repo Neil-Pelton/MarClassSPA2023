6ac2814f61e6aa89f215acc9cbeeba6a7e42e905
Neil-Pelton <neilpel22@gmail.com>
2023-05-01 21:39:02 -0500

    7.1

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { Header, Nav, Main, Footer } from "./components";
 import * as store from "./store";
 import Navigo from "navigo";
 import { after, capitalize } from "lodash";
+import axios from "axios";
 
 const router = new Navigo("/");
 
@@ -24,6 +25,37 @@ function afterRedner(state) {
     document.querySelector("nav > ul").classList.toggle("hidden--mobile");
   });
 }
+
+router.hooks({
+  before: (done, params) => {
+    const view = params && params.data && params.data.view ? capitalize(params.data.view) : "Home"; //happend before the page is loaded
+    // Add a switch case statement to handle multiple routes
+    switch (view) {
+      case "Pizza":
+        // New Axios get request utilizing already made environment variable
+        axios
+          .get(`https://sc-pizza-api.onrender.com/pizzas`)
+          .then(response => {
+            // We need to store the response to the state, in the next step but in the meantime let's see what it looks like so that we know what to store from the response.
+            console.log("response", response);
+            store.Pizza.pizzas = response.data; //response.data = array of objects
+            done();
+          })
+          .catch((error) => { //if error occurs, catch "catches" error and returns:
+            console.log("It puked", error);
+            done();
+          });
+          break;
+      default :
+        done();
+    }
+  },
+  already: (params) => {
+    const view = params && params.data && params.data.view ? capitalize(params.data.view) : "Home";
+
+    render(store[view]); //difference between alread and before is this line of code - allows us to make api call when page is already loaded
+  }
+});
 
 router
   .on({
